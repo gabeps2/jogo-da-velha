b0d4ad0cb020ad5ae1e7c8c009fb86b0fad5b3c4
Gabriel <gabriel.psilva226@gmail.com>
2020-10-01 23:03:10 -0300

    .

--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var buttonPlay = document.querySelector('#play');
-buttonPlay === null || buttonPlay === void 0 ? void 0 : buttonPlay.addEventListener("click", play);
-function play() {
-    console.log("Play!");
-}
-var tabuleiro = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-var turn = 0;
-var changeColor = function (pos) {
-    var _a, _b, _c, _d;
-    var xColor = '#00ff00';
-    var oColor = '#f3880d';
-    if (turn == 0) {
-        (_a = document.getElementById('pos0')) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "background-color: " + xColor);
-        (_b = document.getElementById('pos0')) === null || _b === void 0 ? void 0 : _b.setAttribute("style", "background-image: url(\"../src/images/circle.png\");')");
-        turn = 1;
-    }
-    else {
-        (_c = document.getElementById('pos1')) === null || _c === void 0 ? void 0 : _c.setAttribute("style", "background-color: " + oColor);
-        (_d = document.getElementById('pos1')) === null || _d === void 0 ? void 0 : _d.setAttribute("style", "background-image: url(\"../src/images/x.png\");')");
-        turn = 0;
-    }
-    tabuleiro[0] = 1;
-    console.log(tabuleiro);
+//Buttons
+var changeStyleButtons = function (element) {
+    element.style.backgroundColor = '#5100FF';
 };
-var pos0 = document.getElementById('pos0');
-pos0 === null || pos0 === void 0 ? void 0 : pos0.addEventListener('click', changeColor);
+var buttonPvP = document.getElementById('pvp');
+buttonPvP === null || buttonPvP === void 0 ? void 0 : buttonPvP.addEventListener('click', function () {
+});
+//Tabuleiro
+var tabuleiro = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var turn = 1; //Define o turno de cada jogador - 0 = X / 1 = O
+var changeStyle = function (area) {
+    var _a, _b;
+    var xColor = '#0E9DDF';
+    var oColor = '#DF0E0E';
+    var pos = "pos" + area;
+    if (tabuleiro[area - 1] == 0) {
+        tabuleiro[area - 1] = 1;
+        if (turn == 0) {
+            (_a = document.getElementById(pos)) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "background-image: url(\"../src/images/circleicon.png\"); \n            background-color: " + xColor);
+            turn = 1;
+        }
+        else {
+            (_b = document.getElementById(pos)) === null || _b === void 0 ? void 0 : _b.setAttribute("style", "background-image: url(\"../src/images/xicon.png\"); \n            background-color: " + oColor);
+            turn = 0;
+        }
+    }
+};
 var pos1 = document.getElementById('pos1');
-pos1 === null || pos1 === void 0 ? void 0 : pos1.addEventListener('click', changeColor);
+pos1 === null || pos1 === void 0 ? void 0 : pos1.addEventListener('click', function () { return changeStyle(1); });
 var pos2 = document.getElementById('pos2');
+pos2 === null || pos2 === void 0 ? void 0 : pos2.addEventListener('click', function () { return changeStyle(2); });
 var pos3 = document.getElementById('pos3');
+pos3 === null || pos3 === void 0 ? void 0 : pos3.addEventListener('click', function () { return changeStyle(3); });
 var pos4 = document.getElementById('pos4');
+pos4 === null || pos4 === void 0 ? void 0 : pos4.addEventListener('click', function () { return changeStyle(4); });
 var pos5 = document.getElementById('pos5');
+pos5 === null || pos5 === void 0 ? void 0 : pos5.addEventListener('click', function () { return changeStyle(5); });
 var pos6 = document.getElementById('pos6');
+pos6 === null || pos6 === void 0 ? void 0 : pos6.addEventListener('click', function () { return changeStyle(6); });
 var pos7 = document.getElementById('pos7');
+pos7 === null || pos7 === void 0 ? void 0 : pos7.addEventListener('click', function () { return changeStyle(7); });
 var pos8 = document.getElementById('pos8');
+pos8 === null || pos8 === void 0 ? void 0 : pos8.addEventListener('click', function () { return changeStyle(8); });
+var pos9 = document.getElementById('pos9');
+pos9 === null || pos9 === void 0 ? void 0 : pos9.addEventListener('click', function () { return changeStyle(9); });
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,65 @@
 import Jogador from './scripts/Jogador'
 import Tabuleiro from './scripts/Tabuleiro'
 
-const buttonPlay = document.querySelector('#play')
-buttonPlay?.addEventListener("click", play);
 
-function play(this: HTMLElement) {
-    console.log("Play!");
+
+//Buttons
+const changeStyleButtons = (element:HTMLElement) =>{
+    element.style.backgroundColor = '#5100FF';
 }
 
-var tabuleiro = [0,0,0,0,0,0,0,0,0]
+var buttonPvP = document.getElementById('pvp');
+buttonPvP?.addEventListener('click', () =>{
 
-var turn = 0;
+})
 
-const changeColor = (pos:number)=> {
-    var xColor = '#00ff00'
-    var oColor = '#f3880d'
 
-    if(turn == 0){
-        document.getElementById('pos0')?.setAttribute("style", `background-color: ${xColor}`);
-        document.getElementById('pos0')?.setAttribute("style", `background-image: url("../src/images/circle.png");')`);
-        
-        turn = 1;
-    }else{
-        document.getElementById('pos1')?.setAttribute("style", `background-color: ${oColor}`);
-        document.getElementById('pos1')?.setAttribute("style", `background-image: url("../src/images/x.png");')`);
-        turn = 0;
+//Tabuleiro
+var tabuleiro = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+var turn = 1; //Define o turno de cada jogador - 0 = X / 1 = O
+
+const changeStyle = (area: number) => {
+    var xColor = '#0E9DDF'
+    var oColor = '#DF0E0E'
+
+    var pos = "pos" + area;
+
+    if (tabuleiro[area - 1] == 0) {
+        tabuleiro[area - 1] = 1;
+
+        if (turn == 0) {
+            document.getElementById(pos)?.setAttribute("style",
+                `background-image: url("../src/images/circleicon.png"); 
+            background-color: ${xColor}`);
+            turn = 1;
+        } else {
+            document.getElementById(pos)?.setAttribute("style",
+                `background-image: url("../src/images/xicon.png"); 
+            background-color: ${oColor}`);
+            turn = 0;
+        }
     }
-    tabuleiro[0] = 1;
-    console.log(tabuleiro)
-
 }
 
-
-
-var pos0 = document.getElementById('pos0')
-pos0?.addEventListener('click', changeColor)
 var pos1 = document.getElementById('pos1')
-pos1?.addEventListener('click', changeColor)
+pos1?.addEventListener('click', () => changeStyle(1))
 var pos2 = document.getElementById('pos2')
+pos2?.addEventListener('click', () => changeStyle(2))
 var pos3 = document.getElementById('pos3')
+pos3?.addEventListener('click', () => changeStyle(3))
 var pos4 = document.getElementById('pos4')
+pos4?.addEventListener('click', () => changeStyle(4))
 var pos5 = document.getElementById('pos5')
+pos5?.addEventListener('click', () => changeStyle(5))
 var pos6 = document.getElementById('pos6')
+pos6?.addEventListener('click', () => changeStyle(6))
 var pos7 = document.getElementById('pos7')
+pos7?.addEventListener('click', () => changeStyle(7))
 var pos8 = document.getElementById('pos8')
-
+pos8?.addEventListener('click', () => changeStyle(8))
+var pos9 = document.getElementById('pos9')
+pos9?.addEventListener('click', () => changeStyle(9))
 
 
 
