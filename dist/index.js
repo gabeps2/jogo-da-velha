879c119d460b76578e9e6e26628e77cba21a4bda
gabeps2 <gabeon@outlook.com>
2020-10-04 14:49:24 -0300

    random plays

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var es6_promise_1 = require("es6-promise");
+require("webpack");
 //Buttons
 var changeStyleButtons = function (element) {
     element.style.backgroundColor = '#5100FF';
@@ -10,15 +12,21 @@ buttonPvP === null || buttonPvP === void 0 ? void 0 : buttonPvP.addEventListener
 //Elements HTML
 var body = document.getElementById('body');
 var round = document.getElementById('round-now');
+var roundText = document.getElementById('round-text');
 //Tabuleiro
 var tabuleiro = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 //Tabuleiro com valor 0 = posição vazia
 //Tabuleiro com valor 1 = posição marcada com X
 //Tabuleiro com valor 2 = posição marcada com O
+var numFreePositions = tabuleiro.length; //Número de posições livres no tabuleiro
 var turn = 0; //Define o turno de cada jogador - 0 = X / 1 = O
 var counter = [0, 0]; //Registra o número de jogadas de cada player
 var playerX = 0; //Identificação de cada player
 var playerO = 1;
+var insufficientPlays = 0.;
+var gameInProgress = 1;
+var victory = 2;
+var tie = 3;
 var checkVitory = function (player) {
     //Returns: 0 = insufficient plays
     //         1 = game in progess
@@ -26,20 +34,18 @@ var checkVitory = function (player) {
     //         3 = tie (empate)
     if (counter[player] < 3)
         return 0;
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 3; i++) { //Verifica as linhas
         var count = 0;
         for (var j = 0; j < 3; j++) {
             if (tabuleiro[i][j] == player)
                 count++;
-            console.log("valor de i: " + i);
-            console.log("valor de j: " + j);
             if (count == 3) {
                 console.log("Victory!");
                 return 2;
             }
         }
         count = 0;
-        for (var j = 0; j < 3; j++) {
+        for (var j = 0; j < 3; j++) { //Verifica as colunas
             if (tabuleiro[j][i] == player) {
                 count++;
             }
@@ -49,6 +55,20 @@ var checkVitory = function (player) {
             }
         }
     }
+    count = 0;
+    for (var i = 0; i < 3; i++) { //Verifica a diagonal principal
+        var j = i;
+        if (tabuleiro[i][j] == player) {
+            count++;
+        }
+        if (count == 3) {
+            console.log("Victory! 3");
+            return 2;
+        }
+    }
+    if (counter[0] + counter[1] == 9)
+        return 3;
+    return 1;
 };
 var changeStyle = function (line, column, id) {
     var _a, _b;
@@ -58,20 +78,36 @@ var changeStyle = function (line, column, id) {
     if (tabuleiro[line][column] == 0) {
         if (turn == 0) {
             tabuleiro[line][column] = 2; //Marca a posição com um X
-            counter[0] += 1;
-            checkVitory(playerX);
-            console.log(tabuleiro);
+            counter[playerX] += 1;
             (_a = document.getElementById(pos)) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "background-image: url(\"../src/images/xicon-red.png\"); \n            background-color: " + xColor);
+            if (checkVitory(2) == victory) {
+                round === null || round === void 0 ? void 0 : round.setAttribute("style", 'background-color: #42f563; background-image: url("../src/images/xicon-dark.png")');
+                roundText === null || roundText === void 0 ? void 0 : roundText.setAttribute("data-content", "Victory!");
+                return true;
+            }
+            else if (checkVitory(2) == tie) {
+                round === null || round === void 0 ? void 0 : round.setAttribute("style", 'background-color: #ffea00; background-image: url("../src/images/r2d2.png")');
+                roundText === null || roundText === void 0 ? void 0 : roundText.setAttribute("data-content", "Empate!");
+                return true;
+            }
             body === null || body === void 0 ? void 0 : body.setAttribute("style", '--selected-area-img: url("../src/images/circleicon.png")');
             round === null || round === void 0 ? void 0 : round.setAttribute("style", 'background-image: url("../src/images/circleicon.png"); background-color: #ff4655');
             turn = 1;
         }
         else {
             tabuleiro[line][column] = 1; //Marca a posição com um O
-            counter[1] += 1;
-            checkVitory(playerO);
-            console.log(tabuleiro);
+            counter[playerO] += 1;
             (_b = document.getElementById(pos)) === null || _b === void 0 ? void 0 : _b.setAttribute("style", "background-image: url(\"../src/images/circleicon-dark.png\"); \n            background-color: " + oColor);
+            if (checkVitory(1) == victory) {
+                round === null || round === void 0 ? void 0 : round.setAttribute("style", 'background-color: #42f563; background-image: url("../src/images/circleicon-dark.png")');
+                roundText === null || roundText === void 0 ? void 0 : roundText.setAttribute("data-content", "Victory!");
+                return true;
+            }
+            else if (checkVitory(1) == tie) {
+                round === null || round === void 0 ? void 0 : round.setAttribute("style", 'background-color: #ffea00; background-image: url("../src/images/r2d2.png")');
+                roundText === null || roundText === void 0 ? void 0 : roundText.setAttribute("data-content", "Empate!");
+                return true;
+            }
             body === null || body === void 0 ? void 0 : body.setAttribute("style", '--selected-area-img: url("../src/images/xicon.png")');
             round === null || round === void 0 ? void 0 : round.setAttribute("style", 'background-image: url("../src/images/xicon.png"); background-color: #292C31');
             turn = 0;
@@ -79,7 +115,7 @@ var changeStyle = function (line, column, id) {
     }
 };
 var getNumRandom = function () {
-    var numRandom = Math.random() * (9 - 0) + 0;
+    var numRandom = Math.random() * (10 - 1) + 1;
     return (Math.floor(numRandom));
 };
 var randomPlay = function () {
@@ -87,13 +123,34 @@ var randomPlay = function () {
     var contains;
     do {
         var numRandom = getNumRandom();
+        contains = false;
         for (var i = 0; i < 9; i++) {
             if (numRandom == vetRandomNumbers[i])
                 contains = true;
         }
-    } while (contains);
+    } while (contains && numFreePositions > 0);
+    numFreePositions--;
+    var positions = new Array(10);
+    positions[1] = { line: 0, column: 0 };
+    positions[2] = { line: 0, column: 1 };
+    positions[3] = { line: 0, column: 2 };
+    positions[4] = { line: 1, column: 0 };
+    positions[5] = { line: 1, column: 1 };
+    positions[6] = { line: 1, column: 2 };
+    positions[7] = { line: 2, column: 0 };
+    positions[8] = { line: 2, column: 1 };
+    positions[9] = { line: 2, column: 2 };
+    var numRep = 1;
+    if (numRep = 1) {
+        changeStyle(positions[numRandom].line, positions[numRandom].column, numRandom);
+        numRep = 0;
+    }
 };
-//Verifica as posições
+function sleep(ms) {
+    return new es6_promise_1.Promise(function (resolve) { return setTimeout(randomPlay, ms); });
+}
+sleep(3000);
+//Verifica os eventos nas posições
 var pos1 = document.getElementById('pos1');
 pos1 === null || pos1 === void 0 ? void 0 : pos1.addEventListener('click', function () { return changeStyle(0, 0, 1); });
 var pos2 = document.getElementById('pos2');
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import Jogador from './scripts/Jogador'
 import Tabuleiro from './scripts/Tabuleiro'
+import { Promise } from 'es6-promise';
+import 'webpack'
 
-
-
+interface Positions {
+    line: number;
+    column: number;
+}
 //Buttons
 const changeStyleButtons = (element: HTMLElement) => {
     element.style.backgroundColor = '#5100FF';
@@ -16,6 +20,7 @@ buttonPvP?.addEventListener('click', () => {
 //Elements HTML
 var body = document.getElementById('body');
 var round = document.getElementById('round-now');
+var roundText = document.getElementById('round-text')
 
 
 
@@ -27,6 +32,8 @@ var tabuleiro = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 //Tabuleiro com valor 1 = posição marcada com X
 //Tabuleiro com valor 2 = posição marcada com O
 
+var numFreePositions = tabuleiro.length; //Número de posições livres no tabuleiro
+
 var turn = 0; //Define o turno de cada jogador - 0 = X / 1 = O
 
 var counter = [0, 0]; //Registra o número de jogadas de cada player
@@ -34,7 +41,12 @@ var counter = [0, 0]; //Registra o número de jogadas de cada player
 const playerX = 0; //Identificação de cada player
 const playerO = 1;
 
-const checkVitory = (player: number) => {
+var insufficientPlays = 0.
+var gameInProgress = 1;
+var victory = 2;
+var tie = 3
+
+const checkVitory = (player: number): number => {
 
     //Returns: 0 = insufficient plays
     //         1 = game in progess
@@ -43,20 +55,19 @@ const checkVitory = (player: number) => {
 
     if (counter[player] < 3)
         return 0;
-    for (var i = 0; i < 3; i++) {
+
+    for (var i = 0; i < 3; i++) { //Verifica as linhas
         var count = 0;
         for (var j = 0; j < 3; j++) {
             if (tabuleiro[i][j] == player)
                 count++;
-            console.log("valor de i: " + i)
-            console.log("valor de j: " + j)
             if (count == 3) {
                 console.log("Victory!")
                 return 2;
             }
         }
         count = 0;
-        for (var j = 0; j < 3; j++) {
+        for (var j = 0; j < 3; j++) { //Verifica as colunas
             if (tabuleiro[j][i] == player) {
                 count++;
             }
@@ -66,6 +77,21 @@ const checkVitory = (player: number) => {
             }
         }
     }
+    count = 0;
+    for (var i = 0; i < 3; i++) {//Verifica a diagonal principal
+        var j = i;
+        if (tabuleiro[i][j] == player) {
+            count++
+        }
+        if (count == 3) {
+            console.log("Victory! 3")
+            return 2;
+        }
+    }
+    if (counter[0] + counter[1] == 9)
+        return 3;
+
+    return 1;
 }
 
 const changeStyle = (line: number, column: number, id: number) => {
@@ -77,31 +103,47 @@ const changeStyle = (line: number, column: number, id: number) => {
     if (tabuleiro[line][column] == 0) {
         if (turn == 0) {
             tabuleiro[line][column] = 2;//Marca a posição com um X
-            counter[0] += 1;
-
-            checkVitory(playerX);
-            console.log(tabuleiro);
+            counter[playerX] += 1;
 
             document.getElementById(pos)?.setAttribute("style",
                 `background-image: url("../src/images/xicon-red.png"); 
             background-color: ${xColor}`);
 
+            if (checkVitory(2) == victory) {
+                round?.setAttribute("style", 'background-color: #42f563; background-image: url("../src/images/xicon-dark.png")')
+                roundText?.setAttribute("data-content", "Victory!");
+                return true;
+            }
+            else if (checkVitory(2) == tie) {
+                round?.setAttribute("style", 'background-color: #ffea00; background-image: url("../src/images/r2d2.png")')
+                roundText?.setAttribute("data-content", "Empate!");
+                return true;
+            }
 
             body?.setAttribute("style", '--selected-area-img: url("../src/images/circleicon.png")')
 
             round?.setAttribute("style", 'background-image: url("../src/images/circleicon.png"); background-color: #ff4655');
 
             turn = 1;
+
         } else {
             tabuleiro[line][column] = 1;//Marca a posição com um O
-            counter[1] += 1;
-
-            checkVitory(playerO);
-            console.log(tabuleiro);
+            counter[playerO] += 1;
 
             document.getElementById(pos)?.setAttribute("style",
                 `background-image: url("../src/images/circleicon-dark.png"); 
             background-color: ${oColor}`);
+
+            if (checkVitory(1) == victory) {
+                round?.setAttribute("style", 'background-color: #42f563; background-image: url("../src/images/circleicon-dark.png")')
+                roundText?.setAttribute("data-content", "Victory!");
+                return true;
+            }
+            else if (checkVitory(1) == tie) {
+                round?.setAttribute("style", 'background-color: #ffea00; background-image: url("../src/images/r2d2.png")')
+                roundText?.setAttribute("data-content", "Empate!");
+                return true;
+            }
 
             body?.setAttribute("style", '--selected-area-img: url("../src/images/xicon.png")')
 
@@ -113,26 +155,55 @@ const changeStyle = (line: number, column: number, id: number) => {
 }
 
 const getNumRandom = () => {
-    const numRandom = Math.random() * (9 - 0) + 0;
+    const numRandom = Math.random() * (10 - 1) + 1;
     return (Math.floor(numRandom));
 }
 
 const randomPlay = () => {
 
     var vetRandomNumbers = new Array(9);
-    
+
     var contains;
 
     do {
         var numRandom = getNumRandom();
+        contains = false;
         for (var i = 0; i < 9; i++) {
             if (numRandom == vetRandomNumbers[i])
                 contains = true;
         }
-    } while (contains)
+    } while (contains && numFreePositions > 0)
+
+    numFreePositions--;
+
+    const positions: Positions[] = new Array(10);
+    positions[1] = { line: 0, column: 0 }
+    positions[2] = { line: 0, column: 1 }
+    positions[3] = { line: 0, column: 2 }
+    positions[4] = { line: 1, column: 0 }
+    positions[5] = { line: 1, column: 1 }
+    positions[6] = { line: 1, column: 2 }
+    positions[7] = { line: 2, column: 0 }
+    positions[8] = { line: 2, column: 1 }
+    positions[9] = { line: 2, column: 2 }
+
+    var numRep = 1;
+    if (numRep = 1) {
+        changeStyle(positions[numRandom].line, positions[numRandom].column, numRandom);
+        numRep = 0;
+    }
 }
 
-//Verifica as posições
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(randomPlay, ms));
+}
+
+sleep(3000)
+
+
+
+//Verifica os eventos nas posições
 var pos1 = document.getElementById('pos1')
 pos1?.addEventListener('click', () => changeStyle(0, 0, 1))
 var pos2 = document.getElementById('pos2')
